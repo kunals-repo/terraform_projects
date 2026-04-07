9d1c41277270943849f46ab1002025e03798d5e5
Kunal Yeole <kunalpatil9324@gmail.com>
2026-04-07 06:39:14 +0530

    virtual_network_created

--- main.tf
+++ main.tf
@@ -4,3 +4,9 @@ resource "azurerm_resource_group" "rg-block" {
   location = "eastus"
   
 }
+resource "azurerm_virtual_network" "vnet-block" {
+  name = "vnet1"
+  resource_group_name = azurerm_resource_group.rg-block.name
+  location = azurerm_resource_group.rg-block.location
+  address_space = ["10.0.0.0/24"]
+}
